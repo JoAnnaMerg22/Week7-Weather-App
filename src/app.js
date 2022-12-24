ed70170a7dcec3280d1b3824a90cf3667e24e056
JoAnnaMerg22 <115831892+JoAnnaMerg22@users.noreply.github.com>
2022-12-24 15:50:31 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -29,12 +29,18 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/04d@2x.png`
+  );
 }
 
 let apiKey = "59c311fa9a591a49217a726af72b661d";
