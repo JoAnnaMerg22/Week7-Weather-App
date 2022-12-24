69e24d2fa4e136384243ee123a9bf286bfb3bf7c
JoAnnaMerg22 <115831892+JoAnnaMerg22@users.noreply.github.com>
2022-12-24 14:20:34 -0500

    showing temp,

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "59c311fa9a591a49217a726af72b661d";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
-axios.get(url).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
