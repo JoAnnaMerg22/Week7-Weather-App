1b5d07901f89f31117f811ff3d68fa6a67f476a9
JoAnnaMerg22 <115831892+JoAnnaMerg22@users.noreply.github.com>
2022-12-26 13:23:01 -0500

    added correct icon

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
-    hours = `0%{hours}`;
+    hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0%{minutes}`;
+    minutes = `0${minutes}`;
   }
   let days = [
     "Sunday",
@@ -39,8 +39,9 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/04d@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "59c311fa9a591a49217a726af72b661d";
